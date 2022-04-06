eb229a734f985915b3274fbbc769f3fb9f90d2cb
jungteak <jungteak@DESKTOP-83GI4Q1>
2022-04-06 09:21:48 +0900

    0406

--- MovieProject/src/main/java/movie/pro/MovieProjectApplication.java
+++ MovieProject/src/main/java/movie/pro/MovieProjectApplication.java
@@ -2,12 +2,19 @@ package movie.pro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MovieProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieProjectApplication.class, args);
+	}
+	
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }
